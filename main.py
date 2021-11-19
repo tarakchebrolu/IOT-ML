efe59218a3d8a77f7b1d28b5c83e468df41d3921
tarakchebrolu <tarak05032002@gmail.com>
2021-11-19 14:28:48 +0530

    modified string

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 n = input("enter a number : ")
-print("palindrome number " if n == n[::-1] else: "Not a palindrome number")
+print("palindrome number " if n == n[::-1] else "Not a palindrome number")
